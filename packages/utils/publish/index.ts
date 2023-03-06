c6918be521bd857e324f2ab4628fd71857921d51
lixiaoyanqq <lixy_9791@163.com>
2023-03-06 14:03:53 +0800

    打包配置

--- packages/utils/publish/index.ts
+++ packages/utils/publish/index.ts
@@ -4,11 +4,13 @@
  * @Author: dbkey
  * @Date: 2023-03-06 11:23:44
  * @LastEditors: dbkey
- * @LastEditTime: 2023-03-06 11:33:41
+ * @LastEditTime: 2023-03-06 14:01:47
  */
 import run from '../../components/script/utils/run';
+// import { pkgPath } from '../../../utils/paths'
+import { pkgPath } from '../../components/script/utils/paths'
 import { series } from 'gulp';
 export const publishComponent = async () => {
-  run('release-it', `.`);
+  run('release-it', `${pkgPath}/@ydzy-ui/utils`);
 };
 export default series(async () => publishComponent());
